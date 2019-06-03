1873a2c580c273c7890473c3005c180500d8eda1
YeonwooSung <neos960518@gmail.com>
2019-06-03 10:22:18 +0900

    implement the push notification

--- components/pushNotification.js
+++ components/pushNotification.js
@@ -1,9 +1,18 @@
 import { Permissions, Notifications } from 'expo';
+import { Platform } from 'react-native';
 
 const PUSH_ENDPOINT = 'http://172.30.1.28:8080/notification'; //TODO endpoint
 
 const GRANTED = 'granted';
+const ALERT_MSG = "You need to grant the permission for push notification";
+const CHANNEL_ID = 'Hangil_Notification_Channel';
+const CHANNEL_NAME = 'NOTIFICATION NAME';
 
+
+/**
+ * A function to register the push notification.
+ * @param {String} id The ID string.
+ */
 export default async function registerForPushNotificationsAsync(id) {
     const { status: existingStatus } = await Permissions.getAsync(
         Permissions.NOTIFICATIONS
@@ -15,7 +24,7 @@ export default async function registerForPushNotificationsAsync(id) {
      * It will only ask if permissions have not already been determined, because iOS won't necessarily prompt the user a second time.
      */
     if (existingStatus !== GRANTED) {
-        alert("You need to grant the permission for push notification");
+        alert(ALERT_MSG);
         // Android remote notification permissions are granted during the app install, so this will only ask on iOS
         const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
         finalStatus = status;
@@ -23,6 +32,7 @@ export default async function registerForPushNotificationsAsync(id) {
 
     // Stop here if the user did not grant permissions
     if (finalStatus !== GRANTED) {
+        alert(ALERT_MSG);
         return;
     }
 
@@ -30,6 +40,18 @@ export default async function registerForPushNotificationsAsync(id) {
     let token = await Notifications.getExpoPushTokenAsync();
 
     console.log(token);
+
+    /* 
+     * Check if the platform is android.
+     * If so, open a channel to listen the push notification.
+     * Apparently, this is unnecessary for ios.
+     */
+    if (Platform.OS === 'android') {
+        Expo.Notifications.createChannelAndroidAsync(CHANNEL_ID, {
+            name: CHANNEL_NAME,
+            sound: true,
+        });
+    }
 
     // POST the token to your backend server from where you can retrieve it to send push notifications.
     fetch(PUSH_ENDPOINT, {
@@ -47,7 +69,7 @@ export default async function registerForPushNotificationsAsync(id) {
             },
         }),
     }).catch(function (error) {
-        console.log('Error');
-        console.log(error);
+        if (error)
+            console.log('Error');
     });
 }
