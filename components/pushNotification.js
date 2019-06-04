2cf1248441448456f7b6be01ea3c10f4b8c8cd8a
YeonwooSung <neos960518@gmail.com>
2019-06-04 13:43:22 +0900

    change the code structure so that the app does not send the POST request for the push notification every single time

--- components/pushNotification.js
+++ components/pushNotification.js
@@ -16,68 +16,70 @@ const ASYNC_STORAGE_VALUE = 'exist';
  * @param {String} id The ID string.
  */
 export default async function registerForPushNotificationsAsync(id) {
-    const { status: existingStatus } = await Permissions.getAsync(
-        Permissions.NOTIFICATIONS
-    );
-    let finalStatus = existingStatus;
+    let isCreated = await AsyncStorage.getItem(ASYNC_STORAGE_KEY, undefined);
 
-    /**
-     * Check if the notification is granted.
-     * It will only ask if permissions have not already been determined, because iOS won't necessarily prompt the user a second time.
-     */
-    if (existingStatus !== GRANTED) {
-        alert(ALERT_MSG);
-        // Android remote notification permissions are granted during the app install, so this will only ask on iOS
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-        finalStatus = status;
-    }
+    // check if the channel is created
+    if (!isCreated) {
 
-    // Stop here if the user did not grant permissions
-    if (finalStatus !== GRANTED) {
-        alert(ALERT_MSG);
-        return;
-    }
+        // get the permission status for the notifications
+        const { status: existingStatus } = await Permissions.getAsync(
+            Permissions.NOTIFICATIONS
+        );
 
-    // Get the token that uniquely identifies this device
-    let token = await Notifications.getExpoPushTokenAsync();
+        let finalStatus = existingStatus;
 
-    console.log(token);
+        /**
+         * Check if the notification is granted.
+         * It will only ask if permissions have not already been determined, because iOS won't necessarily prompt the user a second time.
+         */
+        if (existingStatus !== GRANTED) {
+            alert(ALERT_MSG);
+            // Android remote notification permissions are granted during the app install, so this will only ask on iOS
+            const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+            finalStatus = status;
+        }
 
-    /* 
-     * Check if the platform is android.
-     * If so, open a channel to listen the push notification.
-     * Apparently, this is unnecessary for ios.
-     */
-    if (Platform.OS === 'android') {
-        let isCreated = AsyncStorage.getItem(ASYNC_STORAGE_KEY, undefined);
+        // Stop here if the user did not grant permissions
+        if (finalStatus !== GRANTED) {
+            alert(ALERT_MSG);
+            return;
+        }
 
-        console.log(isCreated) //TODO need to use this debugging message to test if the AsyncStorage works properly.
+        // Get the token that uniquely identifies this device
+        let token = await Notifications.getExpoPushTokenAsync();
 
-        if (!isCreated) {
+        console.log(token); //TODO debugging message
+
+        /* 
+        * Check if the platform is android.
+        * If so, open a channel to listen the push notification.
+        * Apparently, this is unnecessary for ios.
+        */
+        if (Platform.OS === 'android') {
             Notifications.createChannelAndroidAsync(CHANNEL_ID, {
                 name: CHANNEL_NAME,
                 sound: true,
             });
-
-            await AsyncStorage.setItem(ASYNC_STORAGE_KEY, ASYNC_STORAGE_VALUE);
         }
-    }
 
-    // POST the token to your backend server from where you can retrieve it to send push notifications.
-    fetch(PUSH_ENDPOINT, {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            data: {
-                token: token,
-                user: id
-            }
-        }),
-    }).catch(function (error) {
-        if (error)
-            console.log('Error');
-    });
+        // POST the token to your backend server from where you can retrieve it to send push notifications.
+        fetch(PUSH_ENDPOINT, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                data: {
+                    token: token,
+                    user: id
+                }
+            }),
+        }).catch(function (error) {
+            if (error)
+                console.log('Error');
+        });
+
+        await AsyncStorage.setItem(ASYNC_STORAGE_KEY, ASYNC_STORAGE_VALUE);
+    }
 }
